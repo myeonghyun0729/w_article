9a2bd1ca61bece5c9b74a479c18d03238d6dbd7e
myeonghyun <96726016+myeonghyun0729@users.noreply.github.com>
2025-09-30 22:11:03 +0900

    Add files via upload

---
+++ src/main/java/com/wedding/article/ArticleApplication.java
@@ -0,0 +1,13 @@
+package com.wedding.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleApplication.class, args);
+	}
+
+}
